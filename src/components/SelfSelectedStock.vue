43e15054434d0271dc72057a1ca95821cc1828f4
alibiubiu <15524498417@163.com>
2019-06-06 15:44:27 +0800

    1

--- src/components/SelfSelectedStock.vue
+++ src/components/SelfSelectedStock.vue
@@ -99,27 +99,27 @@
                   align="center">
                 </el-table-column>
                 <el-table-column
-                  prop="todayOpenPrice"
+                  prop="openPrice"
                   label="今开盘"
                   align="center">
                 </el-table-column>
                 <el-table-column
-                  prop="turnover"
+                  prop="tradeAmount"
                   label="成交额"
                   align="center">
                 </el-table-column>
                 <el-table-column
-                  prop="marketValue"
+                  prop="totalMarketCapitalization"
                   label="市值"
                   align="center">
                 </el-table-column>
                 <el-table-column
-                  prop="priceEarningsRatio"
+                  prop="peRatio"
                   label="市盈率"
                   align="center">
                 </el-table-column>
                 <el-table-column
-                  prop="priceToBookRatio"
+                  prop="pbRatio"
                   label="市净率"
                   align="center">
                 </el-table-column>
