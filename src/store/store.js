8e061f116adddef74a1518c3c719ed183dce473c
myHero <zhengkeyuneu@163.com>
2019-06-13 13:57:01 +0800

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -15,13 +15,13 @@ export default new Vuex.Store({
     stockName: '',
     //用于查看详情的公告title
     title: '',
-    temStockId:'',
+    temStockId: '',
     //设置导航栏
     user: {
       userId: '',
       username: '',
     },
-      buyOrSellStock:'',
+    buyOrSellStock: '',
     SDelegateType: [
       {
         value: 1, label: '最优五档即时成交剩余撤销',
@@ -46,130 +46,130 @@ export default new Vuex.Store({
         value: 7, label: '全额成交或撤销',
       }],
   },
-    getters: {
-      getStockList(state) {
-        return state.stockList
-      },
-      getStockId(state) {
-        if (!state.stockId) {
-          state.stockId = sessionStorage.getItem('stockId')
-        }
-        return state.stockId
-      },
-      getStockName(state) {
-        if (!state.stockName) {
-          state.stockName = sessionStorage.getItem('stockName')
-        }
-        return state.stockName
-      },
-      getUsername(state) {
-        if (!state.user.username) {
-          state.user.username = sessionStorage.getItem('username')
-        }
-        return state.user.username
-      },
-      getUserId(state) {
-        if (!state.user.userId) {
-          state.user.userId = sessionStorage.getItem('userId')
-        }
-        return state.user.userId
-      },
-      //路由跳转时判断当前用户权限
-      isLogin(state) {
-        if (!state.user.username) {
-          state.user.username = sessionStorage.getItem('username')
-        }
-        return state.user.username
+  getters: {
+    getStockList(state) {
+      return state.stockList
+    },
+    getStockId(state) {
+      if (!state.stockId) {
+        state.stockId = sessionStorage.getItem('stockId')
       }
+      return state.stockId
     },
-    //定义改变数据的方法
-    mutations: {
-      login(state, payload) {
-        state.isLogin = true
-        state.user.username = payload.username
-        state.user.userId = payload.userId
-        sessionStorage.setItem("username", payload.username)
-        sessionStorage.setItem("userId", payload.userId)
-      },
-      logout(state) {
-        state.isLogin = false
-        state.user.username = ''
-        state.user.userId = ''
-        sessionStorage.removeItem("username");
-        sessionStorage.removeItem("userId");
-        console.log("logout被调用")
-      },
-      register(state, payload) {
-        // state.isLogin = true
-        // state.user.username = payload.username
-        // state.user.userId = payload.userId
-      },
-      stockList: (state, payload) => {
-        state.stockList = payload
-      },
-      stockId: (state, payload) => {
-        state.stockId = payload
-        sessionStorage.setItem("stockId", payload)
-      },
-      stockName: (state, payload) => {
-        state.stockName = payload
-        sessionStorage.setItem("stockName", payload)
-      },
-      title: (state, payload) => {
-        state.title = payload
-      },
-      buyOrSellStock:(state,payload)=>{
-        state.buyOrSellStock = payload
-      },
-      temStockId:(state,payload)=>{
-        state.temStockId = payload
-      },
+    getStockName(state) {
+      if (!state.stockName) {
+        state.stockName = sessionStorage.getItem('stockName')
+      }
+      return state.stockName
     },
-    //在actions中提交mutations的方法达到改变state的目的 .commit()
-    actions: {
-      login(context, payload) {
-
-        let p = new Promise((resolve, reject) => {
-          alert(JSON.stringify(payload))
-          api.http('post', '/api/login', payload).then(res => {
-            payload.userId = res.data
-            console.log(payload)
-            //   payload.push(temp)
-            context.commit('login', payload)
-            resolve(res)
-          })
-            .catch(err => {
-              reject(err)
-            });
-        })
-
-        return p;
-
-
-      },
-      register(context, payload) {
-        let p = new Promise((resolve, reject) => {
-          alert(JSON.stringify(payload))
-          api.http('post', '/api/register', payload).then(res => {
-            context.commit('register', payload)
-            resolve(res)
-          })
-            .catch(err => {
-              reject(err)
-            });
-
-        })
-
-        return p;
-      },
-
-      stockList(context, payload) {
-        api.http('get', '/api/stockList').then(res => {
-          payload = res.data
-          context.commit('stockList', payload)
-        });
-      },
+    getUsername(state) {
+      if (!state.user.username) {
+        state.user.username = sessionStorage.getItem('username')
+      }
+      return state.user.username
+    },
+    getUserId(state) {
+      if (!state.user.userId) {
+        state.user.userId = sessionStorage.getItem('userId')
+      }
+      return state.user.userId
+    },
+    //路由跳转时判断当前用户权限
+    isLogin(state) {
+      if (!state.user.username) {
+        state.user.username = sessionStorage.getItem('username')
+      }
+      return state.user.username
     }
+  },
+  //定义改变数据的方法
+  mutations: {
+    login(state, payload) {
+      state.isLogin = true
+      state.user.username = payload.username
+      state.user.userId = payload.userId
+      sessionStorage.setItem("username", payload.username)
+      sessionStorage.setItem("userId", payload.userId)
+    },
+    logout(state) {
+      state.isLogin = false
+      state.user.username = ''
+      state.user.userId = ''
+      sessionStorage.removeItem("username");
+      sessionStorage.removeItem("userId");
+      console.log("logout被调用")
+    },
+    register(state, payload) {
+      // state.isLogin = true
+      // state.user.username = payload.username
+      // state.user.userId = payload.userId
+    },
+    stockList: (state, payload) => {
+      state.stockList = payload
+    },
+    stockId: (state, payload) => {
+      state.stockId = payload
+      sessionStorage.setItem("stockId", payload)
+    },
+    stockName: (state, payload) => {
+      state.stockName = payload
+      sessionStorage.setItem("stockName", payload)
+    },
+    title: (state, payload) => {
+      state.title = payload
+    },
+    buyOrSellStock: (state, payload) => {
+      state.buyOrSellStock = payload
+    },
+    temStockId: (state, payload) => {
+      state.temStockId = payload
+    },
+  },
+  //在actions中提交mutations的方法达到改变state的目的 .commit()
+  actions: {
+    login(context, payload) {
 
-  })
+      let p = new Promise((resolve, reject) => {
+        alert(JSON.stringify(payload))
+        api.http('post', '/api/login', payload).then(res => {
+          payload.userId = res.data
+          console.log(payload)
+          //   payload.push(temp)
+          context.commit('login', payload)
+          resolve(res)
+        })
+          .catch(err => {
+            reject(err)
+          });
+      })
+
+      return p;
+
+
+    },
+    register(context, payload) {
+      let p = new Promise((resolve, reject) => {
+        alert(JSON.stringify(payload))
+        api.http('post', '/api/register', payload).then(res => {
+          context.commit('register', payload)
+          resolve(res)
+        })
+          .catch(err => {
+            reject(err)
+          });
+
+      })
+
+      return p;
+    },
+
+    stockList(context, payload) {
+      api.http('get', '/api/stockList').then(res => {
+        payload = res.data
+        context.commit('stockList', payload)
+      });
+    },
+  }
+
+})
 
