dd6366baee6c344f1f2ed1658fc32c48d80532c3
alibiubiu <15524498417@163.com>
2019-06-25 16:44:48 +0800

    guidenav

--- src/components/Guide.vue
+++ src/components/Guide.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style=" margin: 0px; padding: 0px;">
+  <div >
 
     <div id="navigator">
       <el-image :src="url" style="height: 10%;width: 15%;float: left;margin-left: 3%;margin-top: 1%"></el-image>
@@ -13,13 +13,13 @@
                style=" background:rgba(0, 0, 0, 0); width: 60%;float: right;">
 
         <el-menu-item index="AfterLogin" @click="toFirst">首页</el-menu-item>
-        <el-submenu index="3">
+        <el-submenu  index="3">
           <template slot="title">行情中心</template>
           <el-menu-item index="StockList">股票列表</el-menu-item>
           <el-menu-item index="Rank">排行榜</el-menu-item>
         </el-submenu>
-        <el-menu-item  @click="toRouterOrAlert('BuyAtLimitPrice')">股票买卖</el-menu-item>
-        <el-menu-item  index="Guide">股票指南</el-menu-item>
+        <el-menu-item @click="toRouterOrAlert('BuyAtLimitPrice')">股票买卖</el-menu-item>
+        <el-menu-item index="Guide">股票指南</el-menu-item>
         <el-submenu  index="1">
           <template slot="title">信息统计</template>
           <el-menu-item @click="toRouterOrAlert('TodayExchange')">当日成交</el-menu-item>
@@ -27,7 +27,7 @@
           <el-menu-item @click="toRouterOrAlert('HistoryHoldPositionInfo')">历史持仓</el-menu-item>
           <el-menu-item @click="toRouterOrAlert('HistoryExchangeInfo')">历史成交</el-menu-item>
         </el-submenu>
-        <el-menu-item  @click="toRouterOrAlert('SelfCenter')">个人中心</el-menu-item>
+        <el-menu-item @click="toRouterOrAlert('SelfCenter')">个人中心</el-menu-item>
         <el-submenu v-if="this.$store.getters.isLogin" style="padding-left: 4%" index="2">
           <template slot="title"><span style="color: #409EFF;margin: auto;font-size: 6px">欢迎您！{{this.$store.getters.getUsername}}</span>
           </template>
@@ -49,20 +49,20 @@
               <span style="font-size:30px" sortable>交易规则</span>
               <el-divider content-position="right" style="width: 100%"></el-divider>
               <div style=" margin-top:10%;margin-left:5%;margin-right: 5%;text-align: left">
-                <p>
+                <p class="choup">
                   一、交易时间
                   周一至周五 (法定休假日除外)
                   上午9：30 --11：30
                   下午1：00 -- 3：00
                 </p>
-                <p>
+                <p class="choup">
                   二、竞价成交
                   (1) 竞价原则：价格优先、时间优先。价格较高的买进委托优先于价格较低买进委托，价
                   格较低卖出委托优先于较高的卖出委托；同价位委托，则按时间顺序优先。
                   (2) 竞价方式：上午9：15--9：25进行集合竞价 (集中一次处理全部有效委托)；上午
                   9：30--11：30、下午1：00--3：00进行连续竞价 (对有效委托逐笔处理)。
                 </p>
-                <p>
+                <p class="choup">
                   三、交易单位
                   (1) 股票的交易单位为“股”，100股＝1手，委托买入数量必须为100股或其整数倍；
                   (2) 基金的交易单位为“份”，100份＝1手，委托买入数量必须为100份或其整数倍；
@@ -71,29 +71,29 @@
                   (4)当委托数量不能全部成交或分红送股时可能出现零股 (不足1手的为零股)，零股只能委
                   托卖出，不能委托买入零股。
                 </p>
-                <p>
+                <p class="choup">
                   四、报价单位
                   股票以“股”为报价单位；基金以“份”为报价单位；债券以“手”为报价单位。例：行情
                   显示“深发展A”30元，即“深发展A”股现价30元／股。
                   交易委托价格最小变动单位：A股、基金、债券为人民币0.01元；深B为港币0.01元；沪B为
                   美元0.001元；上海债券回购为人民币0.005元。
                 </p>
-                <p>
+                <p class="choup">
                   五、涨跌幅限制
                   在一个交易日内，除首日上市证券外，每只证券的交易价格相对上一个交易日收市价的涨跌
                   幅度不得超过10%，超过涨跌限价的委托为无效委托。
                 </p>
-                <p>
+                <p class="choup">
                   六、"ST"股票
                   在股票名称前冠以“ST”字样的股票表示该上市公司最近两年连续亏损，或亏损一年，但净
                   资产跌破面值、公司经营过程中出现重大违法行为等情况之一，交易所对该公司股票交易进行
                   特别处理。股票交易日涨跌幅限制5%。
                 </p>
-                <p>
+                <p class="choup">
                   七、委托撤单
                   在委托未成交之前，投资者可以撤销委托。
                 </p>
-                <p>
+                <p class="choup">
                   八、"T+1"交收
                   “T”表示交易当天，“T+1”表示交易日当天的第二天。“T+1”交易制度指投资者当天买
                   入的证券不能在当天卖出，需待第二天进行自动交割过户后方可卖出。(债券当天允许“T+0”
@@ -115,143 +115,143 @@
                 <h3>
                   VOL指标（成交量）
                 </h3>
-                <p>
+                <p class="choup">
                   功能名称 VOL指标（成交量）
                 </p>
-                <p>
+                <p class="choup">
                   功能描述 VOL是成交量指标，成交量是指个股或大盘的成交总手
                 </p>
-                <p>
+                <p class="choup">
                   业务规则 VOL是当日实际成交量的反映。
                 </p>
-                <p>
+                <p class="choup">
                   用公式可以表现为:
                 </p>
-                <p>
+                <p class="choup">
                   VOL=∑nVi/N
                 </p>
                 <p>
                   其中,i=1,2,3,......,n；
                 </p>
-                <p>
+                <p class="choup">
                   N=选定的时间参数,如10或30；
                 </p>
-                <p>
+                <p class="choup">
                   Vi:i日成交量。
                 </p>
-                <p>
+                <p class="choup">
                   真实计算 VOL
                 </p>
                 <h3>
                   EXPMA指标
                 </h3>
-                <p>
+                <p class="choup">
                   功能名称 EXPMA指标
                 </p>
-                <p>
+                <p class="choup">
                   功能描述 指数移动平均值,是一种趋向类指标，指数移动平均值是以指数式递减加权的移动平均。
                 </p>
-                <p>
+                <p class="choup">
                   业务规则 12日EMA：EMA（12）=前一日EMA（12）×11/13+今日收盘价×2/13
                 </p>
-                <p>
+                <p class="choup">
                   26日EMA：EMA（26）=前一日EMA（26）×25/27+今日收盘价×2/27
                 </p>
-                <p>
+                <p class="choup">
                   附：
                 </p>
-                <p>
+                <p class="choup">
                   EMA的初值，用第一天的收盘价
                 </p>
-                <p>
+                <p class="choup">
                   真实计算 EMA（12）、EMA（24）
                 </p>
                 <h3>
                   MACD指标
                 </h3>
-                <p>
+                <p class="choup">
                   功能名称 MACD指标
                 </p>
-                <p>
+                <p class="choup">
                   功能描述 MACD是利用收盘价的短期（常用为12日）指数移动平均线与长期（常用为26日）指数移动平均线之间的聚合与分离状况，对买进、卖出时机作出研判的技术指标。
                 </p>
-                <p>
+                <p class="choup">
                   业务规则 快速线为DIF DIF=今日EMA（12）－ 今日EMA（26）
                 </p>
-                <p>
+                <p class="choup">
                   慢速线为DEM 今日DEA（MACD）=前一日DEA×8/10+今日DIF×2/10。
                 </p>
-                <p>
+                <p class="choup">
                   柱状图为MACD （DIF-DEA）×2
                 </p>
-                <p>
+                <p class="choup">
                   真实计算 DIF DEM MACD
                 </p>
                 <h3>
                   W&R指标
                 </h3>
-                <p>
+                <p class="choup">
                   功能名称 W&R指标
                 </p>
-                <p>
+                <p class="choup">
                   功能描述 由Larry williams创造，是一种利用振荡点来反映市场超买超卖现象，预测循环周期内的高点和低点，用来判断市场强弱分界的指标。
                 </p>
-                <p>
+                <p class="choup">
                   业务规则 R=HY-LY
                 </p>
-                <p>
+                <p class="choup">
                   Q=HY-今天的收盘价
                 </p>
-                <p>
+                <p class="choup">
                   W&R=Q/R*100
                 </p>
-                <p>
+                <p class="choup">
                   附：
                 </p>
-                <p>
+                <p class="choup">
                   HY=N天中的最高价
                 </p>
-                <p>
+                <p class="choup">
                   LY=N天中的最低价
                 </p>
-                <p>
+                <p class="choup">
                   N一般取值为6,10
                 </p>
-                <p>
+                <p class="choup">
                   真实计算 W&R（N）） 上限值线80 中间线50 下限值线20
                 </p>
                 <h3>
                   KDJ指标
                 </h3>
 
-                <p>
+                <p class="choup">
                   功能名称 KDJ指标
                 </p>
-                <p>
+                <p class="choup">
                   功能描述
                 </p>
-                <p>
+                <p class="choup">
                   随机指标KDJ一般是用于股票分析的统计体系，根据统计学原理，通过一个特定的周期（常为9日、9周等）内出现过的最高价、最低价及最后一个计算周期的收盘价及这三者之间的比例关系，来计算最后一个计算周期的未成熟随机值RSV，然后根据平滑移动平均线的方法来计算K值、D值与J值，并绘成曲线图来研判股票价格走势。
                 </p>
-                <p>
+                <p class="choup">
                   业务规则 K值：当日K值 = 2/3×前一日K值+1/3×当日RSV
                 </p>
-                <p>
+                <p class="choup">
                   D值：当日D值 = 2/3×前M日D值+1/3×当日K值
                 </p>
-                <p>
+                <p class="choup">
                   J值： 当日J值 = 3*当日K值-2*当日D值
                 </p>
-                <p>
+                <p class="choup">
                   附：
                   1、 先计算：未成熟随机指标值RSV
                   RSV=（Cn－Ln）/（Hn－Ln）×100
                   （Cn为第n日收盘价；Ln为n日内的最低价；Hn为n日内的最高价）
                 </p>
-                <p>
+                <p class="choup">
                   2、 N、M1、M2 天数，一般取9、3、3
                 </p>
-                <p>
+                <p class="choup">
                   3、 若无前一日K 值与D值，则可分别用50来代替。
                   真实计算 K值：当日K值 = M1日平均 RSV
                   D值：当日D值 = M2日平均K值
@@ -261,44 +261,44 @@
                   BIAS指标
                 </h3>
 
-                <p>
+                <p class="choup">
                   功能名称 BIAS指标
                 </p>
-                <p>
+                <p class="choup">
                   功能描述 乖离率(BIAS)，又称偏离率，简称Y值，是通过计算市场指数或收盘价与某条移动平均线之间的差距百分比，以反映一定时期内价格与其MA偏离程度的指标
                 </p>
-                <p>
+                <p class="choup">
                   业务规则 乖离率=（当日收盘价-N日内移动平均价）/N日内移动平均价╳100%
                 </p>
-                <p>
+                <p class="choup">
                   5日乖离率=（当日收盘价-5日内移动平均价）/5日内移动平均价╳100%
                 </p>
-                <p>
+                <p class="choup">
                   BIAS1 : (CLOSE-MA(CLOSE,L1））/MA(CLOSE,L1）*100;
 
                 </p>
-                <p>
+                <p class="choup">
                   BIAS2 : (CLOSE-MA(CLOSE,L2））/MA(CLOSE,L2）*100;
                 </p>
-                <p>
+                <p class="choup">
                   BIAS3 : (CLOSE-MA(CLOSE,L3））/MA(CLOSE,L3）*100
                 </p>
-                <p>
+                <p class="choup">
                   附：
                 </p>
-                <p>
+                <p class="choup">
                   CLOSE为当日收盘价
                 </p>
-                <p>
+                <p class="choup">
                   MA(CLOSE,N)为N日均线的当前数值。
                 </p>
-                <p>
+                <p class="choup">
                   L1 = 6，L2 = 12，L3=24
                 </p>
                 <h3>
                   PSY指标
                 </h3>
-                <p>
+                <p class="choup">
 
                   功能名称 PSY指标
                   功能描述 心理线（PSY）指标将一定时期内投资者趋向买方或卖方的心理事实转化为数值，从而判断股价的未来趋势。
@@ -311,7 +311,7 @@
                 <h3>
                   RSI指标
                 </h3>
-                <p>
+                <p class="choup">
 
                   功能名称 RSI指标
                   功能描述
@@ -464,7 +464,7 @@
     margin-right: 10%;
   }
 
-  p {
+  .choup {
     text-align: left;
     text-indent: 2em;
     line-height: 25px;
@@ -493,8 +493,8 @@
     position: sticky;
     top: 0;
     z-index: 2;
-    display: inline-block;
     background-color: #fff;
+    display: inline-block;
   }
 
 </style>
--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="body">
+    <div id="thisin">
       <div id="fleft">
         <el-image :src="theurl" style="width: 150px;height: 150px"></el-image>
         <p style="color: firebrick">自强不息，知行合一</p>
       </div>
       <div id="fright">
-        <h4>友情链接</h4>
+        <h4 class="h">友情链接</h4>
         <p>
           <a href="http://www.neu.edu.cn/main.htm">东北大学</a>
         </p>
@@ -15,14 +15,14 @@
 
       </div>
       <div id="frightright">
-        <h4>联系我们</h4>
+        <h4 class="h">联系我们</h4>
         <p>
           <i class="el-icon-location-information"></i>
-          <scan>地址：辽宁省沈阳市浑南区智慧大街500号东北大学浑南校区</scan>
+          <scan class="sc">地址：辽宁省沈阳市浑南区智慧大街500号东北大学浑南校区</scan>
         </p>
         <p>
           <i class="el-icon-phone-outline"></i>
-          <scan>188-8888-8888</scan>
+          <scan class="sc">188-8888-8888</scan>
         </p>
 
       </div>
@@ -41,7 +41,7 @@
 </script>
 
 <style scoped>
-  #body{
+  #thisin{
     margin: auto;
     width: 100%;
     display: inline-block;
@@ -65,7 +65,7 @@
     width: 30%;
     margin-left: -20%;
   }
-  p{
+  .sc{
     font-weight: 900;
     font-size: 15px;
     color: #8d8e93;
@@ -81,7 +81,7 @@
     color: #8d8e93;
   }
 
-  h4{
+  .h{
     font-size: 20px;
     color: #fff;
     line-height: 25px;
