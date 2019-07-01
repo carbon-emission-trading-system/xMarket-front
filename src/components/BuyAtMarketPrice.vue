cb9e45cc718cc4976c58c8fbfcb4237fa02ce2e3
myHero <zhengkeyuneu@163.com>
2019-07-01 11:32:27 +0800

    计算

--- src/components/BuyAtMarketPrice.vue
+++ src/components/BuyAtMarketPrice.vue
@@ -492,10 +492,11 @@
        * @return {{articles: {stockId: number, stockName: string, orderPrice: number, canorderAmount: number, soh: number}}}
        */
       CalculatingTax(allFund, price) {
-        if (Math.floor(allFund / (price * 1.030287 * 100)) * 100 * price > 166.6) {
-          return Math.floor(allFund / (price * 1.030287 * 100)) * 100;
+        let res = '';
+        if (Math.floor(allFund / (price * 1.000027 * 100)) * 100 * price > 20000) {
+          res = Math.floor(allFund / (price * 1.000027 * 100)) * 100;
         } else {
-          return Math.floor((allFund - 5) / (price * 1.000287 * 100)) * 100;
+          res = Math.floor((allFund - 5) / (price * 1.00002 * 100)) * 100;
         }
       }
       ,
